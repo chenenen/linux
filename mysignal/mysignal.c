59cb4e671f0686da81d861fa13b6935e3df352c9
chenenen <1145564345@qq.com>
2017-06-03 05:42:15 -0700

    mysignal

---
+++ mysignal/mysignal.c
@@ -0,0 +1,52 @@
+/**************************************
+*文件说明:mysignal.c
+*作者:cherlene
+*创建时间:Thu 01 Jun 2017 05:41:22 PM PDT
+*开发环境:linux 2.6.32-431.el16.x86_64
+****************************************/
+#include<stdio.h>
+#include<signal.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/types.h>
+//void handle(int sig)
+//{
+//	printf("pid:%d, signal:%d\n",getpid(),sig);
+//	
+//}
+//int main()
+//{
+//	signal(2,handle);
+////	while(1)
+////	{}
+//
+////	int i =0;
+////	//i = i/0;
+////	for(i = 0; i<=31; i++)
+////	{
+////		signal(i,handle);
+////	}
+//
+//	//signal(2,SIG_IGN);
+////	signal(2,SIG_DFL);
+////	while(1){
+////		printf("hello\n");
+////	}
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
